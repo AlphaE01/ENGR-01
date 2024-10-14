22109ae1f0daf4e9b9a160608b8ad5e5f7fddd16
TSCR <154628194+AlphaE01@users.noreply.github.com>
2024-10-14 22:00:15 +0800

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,10 @@
 // src/firebaseConfig.js
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCcLZsj89t7hKe7DM8QxjnhUDE_YC858U4",
   authDomain: "adp-engineering-society.firebaseapp.com",
@@ -14,20 +12,17 @@ const firebaseConfig = {
   storageBucket: "adp-engineering-society.appspot.com",
   messagingSenderId: "1019136644050",
   appId: "1:1019136644050:web:3941c08d7680ccb9886e0b",
-  measurementId: "G-HC61T815GC"
+  measurementId: "G-HC61T815GC",
 };
 
-// Initialize Firebase
+// Initialize Firebase App
 const firebaseApp = initializeApp(firebaseConfig);
 
-// Initialize Firebase Analytics (optional)
+// Initialize Firebase Analytics
 const analytics = getAnalytics(firebaseApp);
 
-// Initialize Firestore
+// Initialize Firestore and Storage
 const db = getFirestore(firebaseApp);
-
-// Initialize Storage
 const storage = getStorage(firebaseApp);
 
-// Export Firestore and Storage
-export { db, storage };
+export { firebaseApp, analytics, db, storage };
