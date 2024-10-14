8a4d284bab2765976268273fda251d3aa16c3028
TSCR <154628194+AlphaE01@users.noreply.github.com>
2024-10-15 00:20:19 +0800

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,27 +1,27 @@
-// src/firebaseConfig.js
-
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcLZsj89t7hKe7DM8QxjnhUDE_YC858U4",
-  authDomain: "adp-engineering-society.firebaseapp.com",
-  projectId: "adp-engineering-society",
-  storageBucket: "adp-engineering-society.appspot.com",
-  messagingSenderId: "1019136644050",
-  appId: "1:1019136644050:web:3941c08d7680ccb9886e0b",
-  measurementId: "G-HC61T815GC",
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id",
+  measurementId: "your-measurement-id",
 };
 
-// Initialize Firebase App
 const firebaseApp = initializeApp(firebaseConfig);
 
-// Initialize Firebase Analytics
-const analytics = getAnalytics(firebaseApp);
+let analytics;
+try {
+  analytics = getAnalytics(firebaseApp);
+} catch (error) {
+  console.warn("Firebase Analytics not available: ", error);
+}
 
-// Initialize Firestore and Storage
 const db = getFirestore(firebaseApp);
 const storage = getStorage(firebaseApp);
 
