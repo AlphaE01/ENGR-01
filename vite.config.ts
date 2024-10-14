8d5dfdc90cf751a2d6d78a48d50c0cbaec4cfe71
TSCR <154628194+AlphaE01@users.noreply.github.com>
2024-10-14 21:56:16 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['firebase/app', 'firebase/firestore', 'firebase/storage', 'firebase/auth'],
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 });
